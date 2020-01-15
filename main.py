062d21d4bbaa1ba0658ba7ef9fa8abb6641eabae
KV Le <minecraftsurvivor231@gmail.com>
2020-01-15 03:16:48 -0800

    Implemented A* and Dijkstra's pathfinding algorithms and got a simple visual up and running. Next step is to improve the visuals and refactor code. Maybe commenting too

--- main.py
+++ main.py
@@ -2,9 +2,18 @@
 import math
 import pygame
 import numpy as np
+import sys
+import time
 
-board_height = 10
-board_width = 10
+screen = pygame.display.set_mode((1000, 1000))
+colors = {
+    "wall": (255, 255, 255),
+    "seen": (0, 0, 255),
+    "path": (0, 0, 0),
+    "goal": (0, 255, 0),
+    "start": (255, 0, 0)
+}
+sys.setrecursionlimit(1000000000)
 
 
 class Tile:
@@ -15,6 +24,11 @@ class Tile:
 
     def __repr__(self):
         return f"State {self.state} @ {self.x},{self.y}"
+
+    def update_state(self, state):
+        self.state = state
+        pygame.draw.rect(screen, colors[state], (self.x * tile_width, self.y * tile_height, tile_width, tile_height))
+        pygame.display.update()
 
 
 class AStarQueue(object):
@@ -37,10 +51,40 @@ class AStarQueue(object):
         try:
             min_f_index = 0
             for index in range(len(self.queue)):
-                if self.queue[index].f > self.queue[min_f_index].f:
+                if self.queue[index].f < self.queue[min_f_index].f:
                     min_f_index = index
             deleted_tile = self.queue[min_f_index]
             del self.queue[min_f_index]
+            return deleted_tile
+        except IndexError:
+            print()
+            exit()
+
+
+class DijkstraQueue(object):
+    def __init__(self):
+        self.queue = []
+
+    def __str__(self):
+        return " ".join([str(i) for i in self.queue])
+
+    def __contains__(self, item):
+        return item in self.queue
+
+    def is_empty(self):
+        return len(self.queue) == []
+
+    def insert(self, item):
+        self.queue.append(item)
+
+    def remove(self):
+        try:
+            min_g_index = 0
+            for index in range(len(self.queue)):
+                if self.queue[index].g < self.queue[min_g_index].g:
+                    min_g_index = index
+            deleted_tile = self.queue[min_g_index]
+            del self.queue[min_g_index]
             return deleted_tile
         except IndexError:
             print()
@@ -60,19 +104,19 @@ def get_total_cost(tile):  # f-score for A* path-finding
 
 
 def within_board(x, y):
-    return 0 <= x <= board_width \
-           and 0 <= y <= board_height
+    return 0 <= x < board_width \
+           and 0 <= y < board_height
 
 
 def get_valid_neighbor_coords(x, y):
     neighbors = [
-        [x, y + 1],
-        [x + 1, y + 1],
-        [x + 1, y],
-        [x + 1, y - 1],
         [x, y - 1],
-        [x - 1, y - 1],
+        [x + 1, y],
+        [x, y + 1],
         [x - 1, y],
+        [x + 1, y - 1],
+        [x + 1, y + 1],
+        [x - 1, y - 1],
         [x - 1, y + 1]
     ]
 
@@ -84,25 +128,31 @@ def get_valid_neighbor_coords(x, y):
         if not within_board(x, y):
             neighbors.remove([x, y])
         else:
-            index += 1
+            index = index + 1
 
     return neighbors
 
 
-grid = np.empty((board_width + 1, board_height + 1), object)
-for x_ in range(board_width + 1):
-    for y_ in range(board_height + 1):
+board_height = 100
+board_width = 100
+
+tile_width = 1000 / board_width
+tile_height = 1000 / board_height
+
+grid = np.empty((board_width, board_height), object)
+for x_ in range(board_width):
+    for y_ in range(board_height):
         grid[x_, y_] = Tile(x_, y_)
 
-goal_tile = grid[10, 5]
-goal_tile.state = "goal"
+goal_tile = grid[0, 80]
+goal_tile.update_state("goal")
 
-start_tile = grid[0, 0]
+start_tile = grid[49, 49]
 start_tile.g = 0
-start_tile.state = "seen"
+start_tile.update_state("start")
 
-for x_ in range(board_width + 1):
-    for y_ in range(board_height + 1):
+for x_ in range(board_width):
+    for y_ in range(board_height):
         grid[x_, y_].h = get_distance(grid[x_, y_], goal_tile)
 
 start_tile.f = get_total_cost(start_tile)
@@ -120,7 +170,7 @@ def a_star_search(start: Tile, goal: Tile):
             print(best_tile, "Goal reached")
             return True
 
-        best_tile.state = "seen"
+        best_tile.update_state("seen")
         closed.append(best_tile)
         neighbors = get_valid_neighbor_coords(best_tile.x, best_tile.y)
         print(best_tile)
@@ -143,15 +193,48 @@ def a_star_search(start: Tile, goal: Tile):
     return False
 
 
+def dijkstra_search(start: Tile, goal: Tile):
+    open_queue = DijkstraQueue()
+    open_queue.insert(start)
+    closed = []
+
+    while not open_queue.is_empty():
+        best_tile = open_queue.remove()
+
+        if best_tile.state == "goal":
+            print(best_tile, "Goal reached")
+            return True
+
+        best_tile.update_state("seen")
+        closed.append(best_tile)
+        neighbors = get_valid_neighbor_coords(best_tile.x, best_tile.y)
+        print(best_tile)
+        for x, y in neighbors:
+            if grid[x, y] in closed:
+                continue
+            if not (grid[x, y] in open_queue):
+                grid[x, y].g = best_tile.g + get_distance(grid[x, y], best_tile)
+                open_queue.insert(grid[x, y])
+
+            new_g = best_tile.g + get_distance(grid[x, y], best_tile)
+            if new_g >= grid[x, y].g:
+                continue
+
+            grid[x, y].parent = best_tile
+            grid[x, y].g = new_g
+
+    return False
+
+
 def depth_first_search(current_tile: Tile, goal: Tile):
     neighbors = get_valid_neighbor_coords(current_tile.x, current_tile.y)
 
     print(current_tile, f"Traveled: {current_tile.g} units")
 
     if current_tile.state == "goal":
-        return current_tile.g
+        return True
 
-    current_tile.state = "seen"
+    current_tile.update_state("seen")
 
     for x, y in neighbors:
         if grid[x, y].state != "seen":
@@ -159,7 +242,14 @@ def depth_first_search(current_tile: Tile, goal: Tile):
             grid[x, y].g = get_traveled(grid[x, y])
             return depth_first_search(grid[x, y], goal)
 
-    return -1
+    return False
 
 
-print(a_star_search(start_tile, goal_tile))
+print(dijkstra_search(start_tile, goal_tile))
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+
+    pygame.display.flip()
